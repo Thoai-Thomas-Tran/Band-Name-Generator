dba50f43ad039821c03923b2b9f4e2f857838425
Thoai Thomas Tran <78046328+Thoai-Thomas-Tran@users.noreply.github.com>
2022-04-22 15:00:21 -0700

    Add files via upload

---
+++ Band_Name_Generator.py
@@ -0,0 +1,7 @@
+#Asks user for city name and pet name and concatenates the two names together to create the band name.
+
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in? ")
+pet = input("What is the name of your childhood pet? ")
+band_name = city + " " + pet
+print("Your Band Name is " + band_name + ".")
